2d2845192cb05631d4a56fbfee5173dcbd39ba03
Whirl <ninja21official@gmail.com>
2021-08-26 19:37:20 +0530

    "untested release support for more commands , gonna be ready by sunday , polished by monday"

--- index.js
+++ index.js
@@ -9,28 +9,35 @@ const client = new Client({
 const { Player, AudioFilters } = require("discord-player");
 const wait = require("util").promisify(setTimeout);
 const db = require("quick.db");
+const { Lyrics } = require("@discord-player/extractor");
+const lyricsFinder = Lyrics.init();
 
 const config = require("./config.json");
 
 const player = new Player(client);
-
-player.on("trackStart", (queue, track) =>
-  queue.metadata.channel.send({
-    embeds: [
-      new MessageEmbed()
-        .setColor(`RANDOM`)
-        .setTitle(`Now Playing`)
-        .setDescription(`**${track.title}**`)
-        .setThumbnail(track.thumbnail)
-        .addField(`Duration`, `${track.duration}s`, true)
-        .addField(`Requested By`, `${track.requestedBy.username}`, true)
-        .addField(`Views`, track.views.toString(), true)
-        .addField(`URL`, `**[Click Here](${track.url})**`)
-        .addField(`ARTIST`, track.author, true)
-        .setFooter(`© ${client.user.username} | Made By Whirl#0021`),
-    ],
-  }),
- lcc.send("Evolution")
+/*
+Trim function
+*/
+const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+player.on(
+  "trackStart",
+  (queue, track) =>
+    queue.metadata.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor(`RANDOM`)
+          .setTitle(`Now Playing`)
+          .setDescription(`**${track.title}**`)
+          .setThumbnail(track.thumbnail)
+          .addField(`Duration`, `${track.duration}s`, true)
+          .addField(`Requested By`, `${track.requestedBy.username}`, true)
+          .addField(`Views`, track.views.toString(), true)
+          .addField(`URL`, `**[Click Here](${track.url})**`)
+          .addField(`ARTIST`, track.author, true)
+          .setFooter(`© ${client.user.username} | Made By Whirl#0021`),
+      ],
+    }),
+  lcc.send("Evolution")
 );
 client.once("ready", () => {
   console.log("I'm ready !");
@@ -38,13 +45,10 @@ client.once("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
-  const lc = db.get(`logschannel_${interaction.guild.id}`);
-  let lcc = interaction.guild.channels.cache.get(lc) // gets the level channel
-
 
   if (interaction.commandName == "play") {
     await interaction.deferReply();
-    
+
     if (!interaction.member.voice.channelId)
       return await interaction.editReply({
         content: "You are not in a voice channel!",
@@ -63,6 +67,7 @@ client.on("interactionCreate", async (interaction) => {
     const queue = player.createQueue(interaction.guild, {
       metadata: {
         channel: interaction.channel,
+        int: interaction,
       },
     });
 
@@ -216,10 +221,8 @@ client.on("interactionCreate", async (interaction) => {
             .map((song, index) => `**${index + 1}** - **${song.title}**`)
             .join("\n")}`
         )
-        .addField('Progress Bar', queue.createProgressBar({ queue: true }))
-        .setFooter(
-          `© Musico 2021| Made By Whirl#0021`
-        );
+        .addField("Progress Bar", queue.createProgressBar({ queue: true }))
+        .setFooter(`© Musico 2021| Made By Whirl#0021`);
       return interaction.reply({ embeds: [queueEmbed] });
     }
   } else if (interaction.commandName == "skip") {
@@ -293,15 +296,16 @@ client.on("interactionCreate", async (interaction) => {
       queue.setFilters({ mono: true });
       await interaction.reply(`:white_check_mark: | - Mono filter enabled !`);
     }
-  }
-  else if (interaction.commandName == 'help') {
+  } else if (interaction.commandName == "help") {
     const help = new MessageEmbed()
       .setColor(`RANDOM`)
       .setTitle(`📖 | Help`)
-      .setDescription("**Here are all my commands**\n" + `play , pause , stop , skip , filter , set ,volume , queue , resume , nowplaying , savesong , ping , eval , invite `)
+      .setDescription(
+        "**Here are all my commands**\n" +
+        `play , pause , stop , skip , filter , set ,volume , queue , resume , nowplaying , savesong , ping , eval , invite `
+      );
     interaction.reply({ embeds: [help] });
-  }
-  else if (interaction.commandName == 'nowplaying') {
+  } else if (interaction.commandName == "nowplaying") {
     if (!interaction.member.voice.channel)
       return interaction.channel.send(
         `:x: | - You're not in a voice channel !`
@@ -324,29 +328,38 @@ client.on("interactionCreate", async (interaction) => {
       );
 
     if (queue) {
-      
-
       const embed = new MessageEmbed()
         .setColor(`RANDOM`)
-        .setTitle('🎶 | Now Playing')
+        .setTitle("🎶 | Now Playing")
         .setDescription(queue.nowPlaying().title)
         .setThumbnail(queue.nowPlaying().thumbnail)
         .addFields(
-          { name: 'Uploader', value: queue.nowPlaying().author, inline: true },
-          { name: 'Duration', value: queue.nowPlaying().duration + "s", inline: true },
-          { name: 'Requested By', value: queue.nowPlaying().requestedBy.username, inline: true },
-          { name: 'Views', value: queue.nowPlaying().views.toString(), inline: true },
-          { name: 'Progress Bar', value: queue.createProgressBar({ timecodes: true }) },
-
-      
+          { name: "Uploader", value: queue.nowPlaying().author, inline: true },
+          {
+            name: "Duration",
+            value: queue.nowPlaying().duration + "s",
+            inline: true,
+          },
+          {
+            name: "Requested By",
+            value: queue.nowPlaying().requestedBy.username,
+            inline: true,
+          },
+          {
+            name: "Views",
+            value: queue.nowPlaying().views.toString(),
+            inline: true,
+          },
+          {
+            name: "Progress Bar",
+            value: queue.createProgressBar({ timecodes: true }),
+          }
         )
 
         .setFooter("© 2021 Musico | Made by Whirl#0021");
       interaction.reply({ embeds: [embed] });
-      
     }
-  }
-  else if (interaction.commandName == 'clearqueue') {
+  } else if (interaction.commandName == "clearqueue") {
     if (!interaction.member.voice.channel)
       return interaction.channel.send(
         `:x: | - You're not in a voice channel !`
@@ -372,26 +385,219 @@ client.on("interactionCreate", async (interaction) => {
       queue.clear();
       return interaction.reply(`:white_check_mark: | - Queue cleared !`);
     }
-  }
-  /*
-  Handling and logging events 
-  
-  */
-  else if (interaction.commandName == 'set') {
-    if (interaction.options.getSubcommand() === 'logs_channel') {
-      const channel = interaction.options.getChannel('channel');
-      if (channel.type !== 'GUILD_TEXT') {
-        return interaction.reply("The chosen channel must be a text channel ")
+  } else if (interaction.commandName == "savesong") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
+
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      let npembed = new MessageEmbed()
+        .setColor(`RANDOM`)
+        .setTitle("⏳ | Saved Song")
+        .setDescription(queue.nowPlaying().title)
+        .setThumbnail(queue.nowPlaying().thumbnail)
+        .addFields(
+          { name: "Uploader", value: queue.nowPlaying().author, inline: true },
+          {
+            name: "Duration",
+            value: queue.nowPlaying().duration + "s",
+            inline: true,
+          },
+          {
+            name: "Requested By",
+            value: queue.nowPlaying().requestedBy.username,
+            inline: true,
+          },
+          {
+            name: "Views",
+            value: queue.nowPlaying().views.toString(),
+            inline: true,
+          },
+          {
+            name: "URL",
+            value: [`Click Here`](queue.nowPlaying().url),
+            inline: true,
+          }
+        )
+
+        .setFooter("© 2021 Musico | Made by Whirl#0021");
+      interaction.user.send({ embeds: [embed] });
+      await interaction.reply(`:white_check_mark: | - Check your Dm's !`);
+    }
+  } else if (interaction.commandName == "seek") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
+
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      let time = interaction.options.getInteger("time");
+      let timecode = time;
+      let seconds = 0;
+      if (timecode.length == 2) {
+        seconds = timecode[0] * 60 + timecode[1];
+      } else if (timecode.length == 3) {
+        seconds = timecode[0] * 3600 + timecode[1] * 60 + timecode[2];
+      } else {
+        return interaction.reply(`:x: | - Invalid timecode !`);
       }
-      if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) { return interaction.reply("You don't have the permission to manage the guild") }
-      try {
-        await db.set(`logschannel_${interaction.guild.id}`, channel.id)
-        interaction.reply(`:white_check_mark: | - Logs channel set to ${channel.name}`);
-      }
-      catch { interaction.reply(`:x: | - An error occured !`) }
+      queue.seek(seconds);
+      return interaction.reply(`:white_check_mark: | - Skipped to ${time} !`);
     }
   }
+  else if (interaction.commandName == "pause") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
 
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      queue.setPaused({ paused: true });
+      return interaction.reply(`:white_check_mark: | - Paused !`);
+    }
+  }
+  else if (interaction.commandName == "resume") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
+
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      queue.setPaused({ paused: false });
+      return interaction.reply(`:white_check_mark: | - Resumed the music!`);
+    }
+  }
+  else if (interaction.commandName == "skipto") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
+
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      let skit = interaction.options.getInteger("position");
+      queue.jump(skit).catch(interaction.reply(`:x: | - Invalid position !`));
+      await interaction.reply(`Jumped to track number ${skit} of queue`)
+    }
+  }
+  else if (interaction.commandName == "lyrics") {
+    if (!interaction.member.voice.channel)
+      return interaction.channel.send(
+        `:x: | - You're not in a voice channel !`
+      );
+
+    if (
+      interaction.guild.me.voice.channel &&
+      interaction.member.voice.channel.id !==
+      interaction.guild.me.voice.channel.id
+    )
+      return interaction.channel.send(
+        `:x: | - You are not in the same voice channel !`
+      );
+
+    const queue = player.getQueue(interaction.guild.id);
+
+    if (!queue)
+      return interaction.reply(
+        `:x: | - There is no music playing  in this guild !`
+      );
+
+    if (queue) {
+      let song = queue.nowPlaying();
+      let lyrics = await lyricsFinder.search(song.title);
+      if (lyrics) {
+        let x = trim(lyrics.lyrics, 1024);
+        let lyricsEmbed = new Discord.MessageEmbed()
+          .setColor(`RANDOM`)
+          .setTitle(`Lyrics for ${song.title}`)
+          .setDescription(x)
+          .setFooter(`© Musico 2021 | Made By Whirl#0021`)
+        await interaction.reply({ embeds: [lyricsembed] });
+      } else {
+        await interaction.reply(`:x: | - No lyrics found for this song !`);
+      }
+    }
+  }
 });
 
 client.login(config.token);
