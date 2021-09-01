8ccbc1d30207c5e657829b255a42d0cf717599cd
Whirl <ninja21official@gmail.com>
2021-09-01 17:16:48 +0530

    🎉| Version 2 out

--- replit.sh
+++ replit.sh
@@ -1,4 +1,4 @@
-# This file is for when you are using replit to run your code.
+# This file is for when you are using replit to run your code .
 # Ignore this if you are not using replit.
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 #run this file just once on startup with your repl , then delete it
